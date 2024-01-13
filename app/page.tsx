61da7759a0c74dc60cfa5ec3e2e126b82c2c00cd
babucarr32 <baboucarry@gmai.com>
2024-01-13 15:10:28 +0000

    Refactor homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import Cards from "./cards/page";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     HOME
-     <Cards />
+      <Cards />
     </main>
-  )
+  );
 }
