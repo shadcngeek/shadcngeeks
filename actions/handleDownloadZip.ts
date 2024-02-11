6c244cd135c16c15de27b53afca9cf7c7ef6bd04
babucarr32 <baboucarry@gmai.com>
2024-02-11 21:56:36 +0000

    redo download component

--- actions/handleDownloadZip.ts
+++ actions/handleDownloadZip.ts
@@ -1,6 +1,6 @@
 const siteUrl =
   process.env.NODE_ENV === "production"
-    ? "https://mysite.vercel.app"
+    ? "https://shadcngeeks.vercel.app"
     : "http://localhost:3000";
 
 const handleGetFileName = (pathName: string) => {
@@ -8,20 +8,19 @@ const handleGetFileName = (pathName: string) => {
   return fileName;
 };
 
-const handleDownloadZip = async () => {
+const handleDownloadZip = async (folderPath: string) => {
   try {
     const response = await fetch(`${siteUrl}/api/download`, {
       headers: { "content-type": "application/zip" },
       method: "POST",
-      body: JSON.stringify({ folderToZip: "components/cards/activities" }),
+      body: JSON.stringify({ folderToZip: folderPath }),
     });
-    console.log(await response.json());
     const blob = await response.blob();
     // Create a temporary URL and initiate the download
     const url = window.URL.createObjectURL(new Blob([blob]));
     const link = document.createElement("a");
     link.href = url;
-    const folderName = handleGetFileName("components/cards/activities");
+    const folderName = handleGetFileName(folderPath);
     link.setAttribute("download", `${folderName}.zip`);
     document.body.appendChild(link);
     link.click();
--- components/download/download.tsx
+++ components/download/download.tsx
@@ -5,12 +5,18 @@ import { Button } from "../ui/button";
 
 import handleDownloadZip from "@/actions/handleDownloadZip";
 
-function Download() {
+interface DownloadTypes {
+  folderPath: string;
+  children: React.ReactNode;
+}
+
+function Download({ children, folderPath }: DownloadTypes) {
   return (
     <div className="w-full relative">
       <div className="pb-1 w-full flex justify-end">
-        <Button onClick={() => handleDownloadZip()}>Download</Button>
+        <Button onClick={() => handleDownloadZip(folderPath)}>Download</Button>
       </div>
+      {children}
     </div>
   );
 }
