56e4c5f211074878027d02a3b232e6b4a238a0ae
babucarr32 <baboucarry@gmai.com>
2024-02-17 14:33:07 +0000

    create classic date

---
+++ app/calender/page.tsx
@@ -0,0 +1,6 @@
+import ClassicCalender from "@/components/calender/classic";
+import React from "react";
+
+export default function Calender() {
+  return <ClassicCalender />;
+}
---
+++ components/calender/classic/header/header.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { format } from "date-fns";
+import { CaptionProps, useNavigation } from "react-day-picker";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+function Header(props: CaptionProps) {
+  const { goToMonth, nextMonth, previousMonth } = useNavigation();
+  return (
+    <div className="w-full dark:border-slate-800 dark:text-white flex border h-20 items-center px-10 rounded-t-xl justify-between">
+      <p className="font-bold text-2xl">
+        {format(props.displayMonth, "MMMM yyyy")}
+      </p>
+
+      <div className="flex gap-2">
+        <Button
+          variant={"outline"}
+          disabled={!previousMonth}
+          onClick={() => previousMonth && goToMonth(previousMonth)}
+        >
+          <ChevronLeft size={15} />
+          <span className="ml-3">Previous</span>
+        </Button>
+        <Button
+          variant={"outline"}
+          disabled={!nextMonth}
+          onClick={() => nextMonth && goToMonth(nextMonth)}
+        >
+          <span className="mr-3">Next</span>
+          <ChevronRight size={15} />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
