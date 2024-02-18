b8536478d18fd95e4a0105eb75fb5a8f46092210
babucarr32 <baboucarry@gmai.com>
2024-02-18 13:13:07 +0000

    refactor code

--- components/www/components/landing-page/index.tsx
+++ components/www/components/landing-page/index.tsx
@@ -1,14 +1,6 @@
 "use client";
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
 
-import { pages } from "../../data/pages";
-
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import handleDownloadZip from "@/actions/handleDownloadZip";
-import handleGetFolderPath from "@/actions/handleGetFolderPath";
 import Wrapper from "../wrapper";
 
 function LandingPage() {
