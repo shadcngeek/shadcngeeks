8fba30488b6876f87e077bd77e88d001851716fb
babucarr32 <baboucarry@gmai.com>
2024-02-19 15:11:43 +0000

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css";
 import { GeistSans } from "geist/font/sans";
 import { Inter, JetBrains_Mono as FontMono } from "next/font/google";
 
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 import MegaNav from "@/components/headers/mega-nav";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -28,6 +30,7 @@ export default function RootLayout({
     <html lang="en" id="mode" className="light">
       <body className={GeistSans.className}>
         <MegaNav>{children}</MegaNav>
+        <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS as string} />
       </body>
     </html>
   );
