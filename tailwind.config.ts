8abd7a3692b112ee33818b7c61341aaea7e5a080
babucarr32 <baboucarry@gmai.com>
2024-02-27 21:47:58 +0000

    configure for uploadify

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/react-uploadify/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
