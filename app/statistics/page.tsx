bd38e98bffad7afb7e275e3840e73af4d78d624c
babucarr32 <baboucarry@gmai.com>
2024-03-03 09:29:14 +0000

    update cards and display statistics

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -4,7 +4,7 @@ import Statistics from "@/components/statistics";
 
 export const metadata: Metadata = {
   title: "Statistics",
-  description: "Examples of statistics built using shadcnui components.",
+  description: "Examples of statistics.",
 };
 
 export default function Page() {
