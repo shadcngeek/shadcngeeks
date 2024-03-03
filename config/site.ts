c1e4fce7961ea6a954badf50d7a3a289cb878f29
babucarr32 <baboucarry@gmai.com>
2024-03-03 21:50:23 +0000

    update meta data

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "shadcngeeks",
+  url: "https://shadcngeeks.vercel.app",
+  ogImage:
+    "https://shadcngeeks.vercel.app/_next/image?url=%2Fimages%2Fwww%2Fog.png&w=1080&q=75",
+  description:
+    "An open-source project that provides you with free beautifully predesigned components you can use in your react projects with no limitation.",
+  links: {
+    github: "https://github.com/shadcngeek/shadcngeeks",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
