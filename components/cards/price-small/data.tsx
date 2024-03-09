aa4191229110a932e0c21d955ee6d8e30ea0a5fe
babucarr32 <baboucarry@gmai.com>
2024-03-09 16:16:31 +0000

    remove unused code

--- components/cards/price-small/data.tsx
+++ components/cards/price-small/data.tsx
@@ -1,8 +1,8 @@
 import { PriceProTypes } from "./type";
 
 export const enterprisePlan: PriceProTypes = {
-  plan: "Enterprise",
   price: "$299.99",
+  plan: "Enterprise",
   title: "For Enterprises",
   buttonName: "Get Started",
   description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
