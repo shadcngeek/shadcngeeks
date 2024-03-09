782e1e53f5e46efda0eb782a9cd26e2c035822fe
babucarr32 <baboucarry@gmai.com>
2024-03-09 15:08:05 +0000

    delete unused options

--- components/cards/price-small/data.tsx
+++ components/cards/price-small/data.tsx
@@ -1,5 +1,3 @@
-import { CheckCircle } from "lucide-react";
-
 import { PriceProTypes } from "./type";
 
 export const enterprisePlan: PriceProTypes = {
@@ -8,18 +6,4 @@ export const enterprisePlan: PriceProTypes = {
   title: "For Enterprises",
   buttonName: "Get Started",
   description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  options: [
-    { icon: <CheckCircle size={15} />, text: "Frees SMS" },
-    { icon: <CheckCircle size={15} />, text: "Unlimited Metrics" },
-    { icon: <CheckCircle size={15} />, text: "Free call" },
-    { icon: <CheckCircle size={15} />, text: "Asset Libraries" },
-    { icon: <CheckCircle size={15} />, text: "Full time support" },
-    { icon: <CheckCircle size={15} />, text: "Unlimited team members" },
-    { icon: <CheckCircle size={15} />, text: "Free video calls" },
-    { icon: <CheckCircle size={15} />, text: "Unlimited collaborators" },
-    { icon: <CheckCircle size={15} />, text: "Media Libraries" },
-    { icon: <CheckCircle size={15} />, text: "Unlimited file storage" },
-    { icon: <CheckCircle size={15} />, text: "Customizable templates" },
-    { icon: <CheckCircle size={15} />, text: "Dedicated account manager" },
-  ],
 };
