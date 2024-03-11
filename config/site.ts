a476bcceb0f47202e3e2e11e61815b786e6ab6b1
babucarr32 <baboucarry@gmai.com>
2024-03-11 19:59:40 +0000

    update og path

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   name: "shadcngeeks",
   url: "https://shadcngeeks.vercel.app",
   ogImage:
-    "https://shadcngeeks.vercel.app/_next/image?url=%2Fimages%2Fwww%2Fog.png&w=1080&q=75",
+    "https://shadcngeeks.vercel.app/_next/image?url=%2Fimages%2Fog.png&w=1080&q=75",
   description:
     "An open-source project that provides you with free beautifully predesigned components you can use in your react projects with no limitation.",
   links: {
