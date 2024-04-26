658478cfe35f4629a970a05bcd2c376064ebfd2b
babucarr32 <baboucarry@gmai.com>
2024-04-26 20:10:28 +0000

    Change max width

--- components/headers/mega-nav/index.tsx
+++ components/headers/mega-nav/index.tsx
@@ -10,19 +10,17 @@ interface HeaderTypes {
 function MegaNav({ children }: HeaderTypes) {
   return (
     <div className="dark:bg-slate-950">
-        <div className="border-b border-slate-200 dark:border-slate-800 dark:bg-slate-950">
-          <div className="py-3 px-4 flex justify-between items-center gap-5 max-w-[1536px] mx-auto">
+      <div className="border-b border-slate-200 dark:border-slate-800 dark:bg-slate-950">
+        <div className="py-3 px-4 flex justify-between items-center gap-5 max-w-[1536px] mx-auto">
           <div className="flex items-center gap-20">
             <Link href={"/"} className="">
               <p className="font-bold text-rose-600 text-2xl">ShadcnGeeks</p>
             </Link>
           </div>
           <SocialLinks />
-          </div>
         </div>
-      <div className="max-w-[1536px] mx-auto">
-        {children}
       </div>
+      <div className="mx-auto">{children}</div>
     </div>
   );
 }
