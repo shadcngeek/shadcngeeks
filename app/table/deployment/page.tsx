45da337c83f23c571ff5bc35ba3b59ddb48996c2
babucarr32 <baboucarry@gmai.com>
2024-05-08 20:08:36 +0000

    Update tables path

--- app/table/deployment/page.tsx
+++ app/table/deployment/page.tsx
@@ -1,4 +1,4 @@
-import DeploymentComponent from "@/components/table/deployments/deployment-component";
+import DeploymentComponent from "@/components/table/deployment/deployment-component";
 
 export default function TasksPage() {
   return <DeploymentComponent />;
